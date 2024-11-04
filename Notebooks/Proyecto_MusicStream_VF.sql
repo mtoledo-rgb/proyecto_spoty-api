b8e003f9da068053f8d6ee67d5a1a83d559fb37d
Zoraida-AP <mzoraida82@gmail.com>
2024-11-04 17:42:25 +0100

    corrección querys sql

--- Notebooks/Proyecto_MusicStream_VF.sql
+++ Notebooks/Proyecto_MusicStream_VF.sql
@@ -41,9 +41,9 @@ CREATE TABLE LAST_FM (
 
 -- 1. ¿Qué género tiene el promedio más alto de reproducciones?
 
-SELECT genero, AVG(playcount) AS promedio_reproducciones
-	FROM LAST_FM
-	GROUP BY genero -- Agrupación por género.
+SELECT genero_musica, AVG(playcount) AS promedio_reproducciones
+	FROM last_fm
+	GROUP BY genero_musica -- Agrupación por género.
 	ORDER BY promedio_reproducciones DESC -- Orden descendente para obtener el mayor promedio.
 	LIMIT 1; -- Solo el género con el promedio más alto, queremos un solo resultado. 
 
@@ -51,42 +51,46 @@ SELECT genero, AVG(playcount) AS promedio_reproducciones
 -- 2. ¿Cuántos artistas pertenecen a cada género en la base de datos y cuál es el género con más artistas?
 
 SELECT genero, COUNT(DISTINCT artista) AS cantidad_artistas -- Usamos distinct para asegurarnos de que cada artista lo contamos SOLO 1 vez por género. 
-	FROM SPOTIPY
+	FROM spotipy
 	GROUP BY genero -- Agrupación por género.
 	ORDER BY cantidad_artistas DESC; -- Orden descendente para obtener el género con más artistas.
 
 -- 3. ¿Cuántos artistas tienen menos de 100,000 reproducciones?
 
 SELECT COUNT(DISTINCT artista) AS artistas_menos_de_100k -- Aquí volvemos a usar DISTINCT para que no cuente las filas con más de 1 canción por género como un artista nuevo. 
-	FROM LAST_FM
+	FROM last_fm
 	WHERE playcount < 100000; -- Condición de menos de 100,000 reproducciones
 
 -- 4. ¿Cuál es el artista con más Listeners?
 
 SELECT artista, MAX(listeners) AS max_listeners -- Hacemos un máx, porque queremos saber el num.mayor. 
-	FROM LAST_FM
+	FROM last_fm
 	GROUP BY artista -- Agrupación por artista.
 	ORDER BY max_listeners DESC -- Orden descendente para el mayor número de oyentes.
 	LIMIT 1; -- Solo 1, el artista con más oyentes.
 
 -- 5. ¿Qué país tiene mas artistas?
 
-SELECT pais_origen, COUNT(DISTINCT artista) AS cantidad_artistas
-	FROM MUSIC_BRAINZ
-	GROUP BY pais_origen -- Agrupación por país de origen.
+SELECT Pais_origen, COUNT(DISTINCT artista) AS cantidad_artistas
+	FROM music_brainz
+	GROUP BY Pais_origen -- Agrupación por país de origen.
 	ORDER BY cantidad_artistas DESC; -- Orden descendente para obtener el país con más artistas.
 
 -- 6. ¿Qué artista estuvo más tiempo en activo y cuántas canciones tiene en el rango extraído de información (2022-2023)?
 
-SELECT s.artista, 
-       (YEAR(mb.fin_actividad) - YEAR(mb.inicio_actividad)) AS tiempo_activo, -- usamos el year para extraer solo el año (y no un datetime). 
+SELECT mb.Artista, 
+       (CASE 
+        WHEN mb.Fecha_fin_act IS NULL THEN 
+            YEAR(CURDATE()) - YEAR(mb.Fecha_ini_act)
+        ELSE 
+            YEAR(mb.Fecha_fin_act) - YEAR(mb.Fecha_ini_act)
+    END) AS tiempo_activo, -- usamos el year para extraer solo el año (y no un datetime). 
        COUNT(s.nombre) AS cantidad_canciones -- Contamos las canciones o álbumes.
-FROM SPOTIPY as s
-INNER JOIN MUSIC_BRAINZ as mb 
-ON s.artista = mb.artista -- Unimos las tablas SPOTIPY y MUSIC_BRAINZ por artista.
-GROUP BY s.artista
-ORDER BY tiempo_activo DESC -- Ordenar de mayor a menor tiempo activo.
-LIMIT 1; -- Obtener solo el artista con más tiempo activo. 
+FROM music_brainz AS mb
+    LEFT JOIN spotipy AS s ON mb.Artista = s.artista 
+GROUP BY mb.Artista
+ORDER BY tiempo_activo DESC
+LIMIT 1;
 
 
 -- ---------------------------------------------------------------------------
@@ -94,39 +98,40 @@ LIMIT 1; -- Obtener solo el artista con más tiempo activo.
 
 -- 1. ¿Cuál es el porcentaje de listeners que escuhan a mujeres VS listeners que esuchan a hombres?  
 
-SELECT identidad, 
-       (SUM(listeners) * 100.0 / (SELECT SUM(listeners) 
-       FROM LAST_FM as lf
-       INNER JOIN MUSIC_BRAINZ as mb
-       ON lf.artista = mb.artista 
-       WHERE identidad IN ('Mujer', 'Hombre'))) AS porcentaje_listeners
-FROM LAST_FM as lf -- ¡¡¡¡Aquí tengo dudas si lo tengo que diferenciar del anterior!!!
-INNER JOIN MUSIC_BRAINZ as mb
-ON lf.artista = mb.artista
-WHERE identidad IN ('Mujer', 'Hombre')
-GROUP BY identidad;
+SELECT 
+    mb.Genero AS genero_artista,
+    SUM(lf.listeners) AS total_listeners,
+    ROUND(SUM(lf.listeners) * 100.0 / (SELECT SUM(listeners) FROM last_fm), 2) AS porcentaje_listeners
+FROM 
+    last_fm lf
+INNER JOIN 
+    music_brainz mb ON lf.artista = mb.Artista
+WHERE 
+    mb.Genero IN ('Hombre', 'Mujer')  -- Filtramos por géneros específicos
+GROUP BY 
+    mb.Genero;
 
 -- 2. ¿Cuáles son las artistas mujeres más escuchadas?
 
-SELECT artista, SUM(playcount) AS total_reproducciones
-	FROM LAST_FM as lf
-	INNER JOIN MUSIC_BRAINZ as mb
-    ON lf.artista = mb.artista
-	WHERE identidad = 'Mujer'
-	GROUP BY artista
-	ORDER BY total_reproducciones DESC
-	LIMIT 10;
+SELECT lf.artista, SUM(playcount) AS total_reproducciones
+	FROM last_fm as lf
+INNER JOIN music_brainz as mb
+         ON lf.artista = mb.Artista
+WHERE mb.genero = 'Mujer'
+GROUP BY lf.artista
+ORDER BY total_reproducciones DESC
+LIMIT 10;
 
 -- 3. ¿En qué género ha crecido más el número de reproducciones de artistas femeninas?
 
-SELECT genero, SUM(playcount) AS total_reproducciones
-	FROM LAST_FM as lf
-	INNER JOIN MUSIC_BRAINZ as mb
-    ON lf.artista = mb.artista
-INNER JOIN SPOTIPY as s
-ON lf.artista = s.artista
-WHERE identidad = 'Mujer'
-GROUP BY genero
+SELECT lf.genero_musica, SUM(playcount) AS total_reproducciones
+FROM last_fm as lf
+INNER JOIN music_brainz as mb
+    ON lf.artista = mb.Artista
+INNER JOIN SPOTIPY as s 
+    ON lf.artista = s.artista
+WHERE mb.genero = 'Mujer'
+GROUP BY lf.genero_musica
 ORDER BY total_reproducciones DESC
 LIMIT 1;
 
